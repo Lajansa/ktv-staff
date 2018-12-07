664f12db0e50bda999e3f0325e245122d3635343
Isa <lajansa@hotmail.com>
2018-12-07 22:03:03 +0800

    Merge conflict

--- src/components/screens/songSelection/song/songItem.js
+++ src/components/screens/songSelection/song/songItem.js
@@ -5,15 +5,26 @@ import { connect } from 'react-redux';
 import db from '../../../../../db.json';
 
 class SongItem extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selected: false
+      }
+    }
+    
     render() {
       const song = db[this.props.categoryID].singers[this.props.singerID].songs[this.props.songID];
+      const iconName = this.state.selected ? 'check' : 'add';
       return (
         <ListItem
           key={this.props.index}
           containerStyle={{paddingLeft: 10, borderBottomColor: '#ddd'}}
           title={this.props.name}
-          rightIcon={{name: 'add'}}
-          onPressRightIcon={() => this.props.addSong(song)}
+          rightIcon={{name: iconName}}
+          onPressRightIcon={() => {
+            this.setState({selected: true});
+            if (!this.state.selected) this.props.addSong(song);
+          }}
         />
       )
     }
